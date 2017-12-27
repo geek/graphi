dbe1c1a3738527ea7a3ebdb8a58ee42129cd625b
Jon Swenson <jswenson74@gmail.com>
2017-12-26 21:46:59 -0500

    added sample curl graphql query to example.js

--- example.js
+++ example.js
@@ -45,6 +45,7 @@ internals.init()
   .then((server) => {
     console.log('server.info.uri ' + server.info.uri);
     // open http://localhost:8000/graphiql?query=%7B%20person(firstname%3A%20%22billy%22)%20%7B%20lastname%20%7D%20%7D&variables=%7B%7D
+    // curl -X POST -H "Content-Type: application/json" -d '{"query":"{person(firstname:\"billy\"){lastname}}"}' http://127.0.0.1:8000/graphql
   })
   .catch((error) => {
     console.log('Error: ' + error);
