cc63f234f337a77094f61d76e125e7870f528da9
geek <wpreul@gmail.com>
2019-03-03 13:17:34 -0600

    Add graphiql tracing test

--- test/index.js
+++ test/index.js
@@ -1820,6 +1820,48 @@ describe('server.registerSchema()', () => {
       expect(res.statusCode).to.equal(200);
       expect(res.result.data.person.lastname).to.equal('jean');
     });
+
+    it('will not show a blank table when there are no requests', async () => {
+      const schema = `
+        type Person {
+          firstname: String!
+          lastname: String!
+          email: String!
+        }
+
+        type Query {
+          person(firstname: String!): Person!
+        }
+      `;
+
+      const getPerson = function (args, request) {
+        expect(args.firstname).to.equal('billy');
+        expect(request.path).to.equal('/graphql');
+        return { firstname: '', lastname: 'jean', email: 'what' };
+      };
+
+      const resolvers = {
+        person: getPerson
+      };
+
+      let output = '';
+      const originalStdout = process.stdout.write;
+      process.stdout.write = (data) => {
+        output += data.toString();
+      };
+
+      const server = Hapi.server();
+      await server.register({ plugin: Graphi, options: { schema, resolvers, tracing: true } });
+      await server.initialize();
+
+      const res = await server.inject({ method: 'GET', url: '/graphiql' });
+      process.stdout.write = originalStdout;
+
+      expect(output).to.not.include('Duration');
+      expect(output).to.not.include('person - email');
+      expect(output).to.not.include('Query');
+      expect(res.statusCode).to.equal(200);
+    });
   });
 });
 
